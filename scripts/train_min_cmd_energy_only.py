d726aa95d3be6ced8462496f127e113184550be8
Boyuan Liang <liangb@berkeley.edu>
2024-02-19 19:10:06 -0800

    added grid search code

--- scripts/train_min_cmd_energy_only.py
+++ scripts/train_min_cmd_energy_only.py
@@ -33,19 +33,19 @@ def train_go1(args):
     # Fixed Auxiliary Rewards.
     Cfg.reward_scales.lin_vel_z = -0.02
     Cfg.reward_scales.ang_vel_xy = -0.001
-    Cfg.reward_scales.feet_slip = 0.0 # -0.04
+    Cfg.reward_scales.feet_slip = 0.0 # -0.0
     Cfg.reward_scales.collision = -5.0
     Cfg.rewards.soft_dof_pos_limit = 0.9
     Cfg.reward_scales.dof_pos_limits = -10.0
     Cfg.reward_scales.torques = 0.0 # -0.0001
     Cfg.reward_scales.dof_vel = 0.0 # -1e-4
     Cfg.reward_scales.dof_acc = 0.0 # -2.5e-7
-    Cfg.reward_scales.action_smoothness_1 = -0.1
-    Cfg.reward_scales.action_smoothness_2 = -0.1
+    Cfg.reward_scales.action_smoothness_1 = -0.0
+    Cfg.reward_scales.action_smoothness_2 = -0.0
 
     # Rewards used in legged gym, but unparticipated here.
     Cfg.reward_scales.feet_air_time = 0.0
-    Cfg.reward_scales.action_rate = -0.01
+    Cfg.reward_scales.action_rate = -0.0
 
     # Unparticipated Rewards.
     Cfg.reward_scales.dof_pos = 0.0
@@ -69,11 +69,19 @@ def train_go1(args):
     Cfg.reward_scales.tracking_contacts_shaped = 0.0
     Cfg.reward_scales.energy = 0.0 # 0.5
     Cfg.reward_scales.energy_sigma = 300.0
-    Cfg.reward_scales.energy_legs = 0.7
-    Cfg.reward_scales.energy_legs_sigma = 100.0
+    Cfg.reward_scales.energy_legs = args.energy
+    Cfg.reward_scales.energy_legs_sigma = args.sigma
     Cfg.reward_scales.energy_expenditure = 0.0
     Cfg.reward_scales.survival = 0.0
     Cfg.reward_scales.base_motion = 0.0
+
+    Cfg.commands.lin_vel_x = [args.train_speed, args.train_speed]
+    Cfg.commands.lin_vel_y = [0.0, 0.0]
+    Cfg.commands.ang_vel_yaw = [0.0, 0.0]
+
+    Cfg.commands.limit_vel_x = [args.train_speed, args.train_speed]
+    Cfg.commands.limit_vel_y = [0.0, 0.0]
+    Cfg.commands.limit_vel_yaw = [0.0, 0.0]
 
     env = VelocityTrackingEasyEnv(sim_device=f'cuda:{args.device}', headless=args.headless, cfg=Cfg)
 
@@ -97,11 +105,14 @@ if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--headless', action="store_true")
     parser.add_argument('--device', default=0, type=int)
+    parser.add_argument('--energy', default=1.2, type=float)
+    parser.add_argument('--sigma', default=100, type=float)
+    parser.add_argument('--train_speed', default=1.0, type=float)
     args = parser.parse_args()
 
     stem = Path(__file__).stem
     logger.configure(
-        logger.utcnow(f'{stem}/%Y-%m-%d-%H%M%S.%f'),
+        logger.utcnow(f'{stem}/energy-{args.energy:.1f}-sigma-{args.sigma:.1f}-speed-{args.train_speed:.1f}'),
         root=Path(f"{MINI_GYM_ROOT_DIR}/checkpoints").resolve()
     )
     logger.log_text("""
